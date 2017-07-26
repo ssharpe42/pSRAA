0dc5c60816a715e0d2fb8aa54d0a575b074c7d0a
ssharpe42 <ssharpe42y@gmail.com>
2017-07-25 21:01:08 -0400

    Deleted old methods

--- cFIP.R
+++ cFIP.R
@@ -12,7 +12,7 @@ dbRetro=dbConnect(MySQL(),user="root",
                   unix.sock="/Applications/MAMP/tmp/mysql/mysql.sock")
 
 #Retrosheet event files
-Season = 2014
+Season = 2016
 pbp = fetch(dbSendQuery(dbRetro, paste0("select * from events_bck where YEAR_ID=",Season)), n = -1)
 pbp[, grepl('_FL$', names(pbp))] = lapply(pbp[, grepl('_FL$', names(pbp))], as.logical)
 IP = group_by(pbp, PIT_ID) %>% summarise(IP = sum(EVENT_OUTS_CT)/3) 
@@ -57,31 +57,6 @@ HR.glmer=readRDS(paste0('/Users/Sam/Desktop/Projects/cFIP/HR.glmer.',Season,'.RD
 HBP.glmer=readRDS(paste0('/Users/Sam/Desktop/Projects/cFIP/HBP.glmer.',Season,'.RDS'))
 K.glmer=readRDS(paste0('/Users/Sam/Desktop/Projects/cFIP/K.glmer.',Season,'.RDS'))
 
-# pbp = mutate(pbp,
-#     BB.int = qnorm(BB.glmer@resp$mu),
-#     HBP.int = qnorm(HBP.glmer@resp$mu),
-#     HR.int = qnorm(HR.glmer@resp$mu),
-#     K.int = qnorm(K.glmer@resp$mu)
-# )
-# group_by(pbp, PIT_ID) %>% 
-#     summarise_each(funs(mean), BB.int, HBP.int, HR.int, K.int)%>%
-#     left_join(., data.frame(PIT_ID = rownames(ranef(BB.glmer)$PIT_ID), 
-#                             BB.r = ranef(BB.glmer)$PIT_ID[,1],
-#                             HBP.r = ranef(HBP.glmer)$PIT_ID[,1], 
-#                             HR.r = ranef(HR.glmer)$PIT_ID[,1], 
-#                             K.r = ranef(K.glmer)$PIT_ID[,1])) %>%
-#     ungroup %>%
-#     mutate(BB = pnorm(BB.int + BB.r)- pnorm(BB.int),
-#            HBP = pnorm(HBP.int + HBP.r)- pnorm(HBP.int),
-#            HR = pnorm(HR.int + HR.r)- pnorm(HR.int),
-#            K = pnorm(K.int + K.r)- pnorm(K.int),
-#            cFIP = 13*HR + 3*(HBP+BB) - 2*K,
-#            cFIPm = ((cFIP - mean(cFIP))/sd(cFIP))*15 + 100)
-# 
-# BB.int = mean(qnorm(BB.glmer@resp$mu))
-# HBP.int = mean(qnorm(HBP.glmer@resp$mu))
-# HR.int = mean(qnorm(HR.glmer@resp$mu))
-# K.int = mean(qnorm(K.glmer@resp$mu))
 
 BB.int = summary(BB.glmer)$coefficients['(Intercept)','Estimate'] 
 HBP.int = summary(HBP.glmer)$coefficients['(Intercept)','Estimate']
